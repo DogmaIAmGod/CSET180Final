61b2c9363818546e07ecf017cae8f575900f5093
tpob11 <tpobursky21@gmail.com>
2023-04-25 15:17:42 -0400

    added signup/login

--- main.py
+++ main.py
@@ -1,71 +1,184 @@
 from flask import Flask, render_template, request
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
-app = Flask(__name__)  # routes 
-conn_str = "mysql://root:Tpob4711$@localhost:3306/finalproject160"
+
+app = Flask(__name__)
+conn_str = "mysql://root:Tpob4711$@localhost/schooltests"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
+
 @app.route('/')
-def index():     return render_template('index.html')
+def index():
+    return render_template('index.html')
 
 
-# @app.route('/teacher_reg', methods= ['GET'])
-# def teacher_registration(): return render_template('teacher_registration.html')
-
-# @app.route('/teacher_reg', methods= ['POST'])
-# def post_teacher_reg():
-#     try:
-
-#         conn.execute(
-#             text('INSERT INTO users (first_name, last_name, email, password) values (:first_name, :last_name, :email, :password)'),
-#             request.form
-#         )
-#         conn.commit()
-#         return render_template('teacher_registration.html', error=None, success= 'Data inserted successfully.')
-#     except Exception as e:
-#         error = e.orig.args[1]
-#         print(error)
-#         return render_template('teacher_registration.html', error=error, success= 'Data inserted successfully.')
-        
+@app.route('/accounts')
+def get_accounts():
+    # student_accounts = conn.execute(text("select * from student_accounts")).all()
+    # teacher_accounts = conn.execute(text("select * from teacher_accounts")).all()
+    # print(student_accounts)
+    # print(teacher_accounts)
+    return render_template('signup_login_page.html')
+                        #    , student_accounts=student_accounts, teacher_accounts=teacher_accounts)
 
 
-
-# @app.route('/student_reg', methods= ['GET'])
-# def student_registration(): return render_template('student_registration.html')
-
-# @app.route('/student_reg', methods= ['POST'])
-# def student_registration(): return render_template('student_registration.html')
-
-
-# @app.route('/student_log')
-# def student_login(): return render_template('student_login.html')
+@app.route('/accounts/accounts_students')
+def get_accounts_student():
+    student_accounts = conn.execute(text("select * from student_accounts")).all()
+    print(student_accounts)
+    return render_template('accounts_students.html', student_accounts=student_accounts)
 
 
-# @app.route('/teacher_log')
-# def teacher_login(): return render_template('teacher_login.html')
+@app.route('/accounts/accounts_teachers')
+def get_accounts_teacher():
+    teacher_accounts = conn.execute(text("select * from teacher_accounts")).all()
+    print(teacher_accounts)
+    return render_template('accounts_teachers.html', teacher_accounts=teacher_accounts)
 
 
-# @app.route('/accounts')
-# def accounts(): return render_template('accounts.html')
+@app.route('/quiz_questions/')
+@app.route('/quiz_questions/<page>')
+def get_boats(page=1):
+    page = int(page)
+    per_page = 10
+    quiz_questions = conn.execute(text(f"SELECT quiz_questions.id, question1, question2, question3, concat(first_name, ' ', last_name) AS teacher_name FROM quiz_questions JOIN teacher_accounts ON teacher_id=teacher_accounts.id LIMIT {per_page} OFFSET {(page - 1) * per_page}")).all()
+    print(quiz_questions)
+    return render_template('boats.html', boats=quiz_questions, page=page, per_page=per_page)
 
 
-# @app.route('/tests', methods= ['GET'])
-# def tests(): return render_template('tests.html')
+@app.route('/teacher_create', methods=['GET'])
+def create_get_request_teacher():
+    return render_template('teacher_create.html')
 
-# @app.route('/tests', methods= ['POST'])
-# def post_tests():
-#     try:
 
-#         conn.execute(
-#             text('INSERT INTO users (title, instructions, question1, question2, question3) values (:test_title, :intructions, :q1, :q2, q3)'),
-#             request.form
-#         )
-#         conn.commit()
-#         return render_template('tests.html', error=None, success= 'Data inserted successfully.')
-#     except Exception as e:
-#         error = e.orig.args[1]
-#         print(error)
-#         return render_template('tests.html', error=error, success= 'Data inserted successfully.')
+@app.route('/teacher_create', methods=['POST'])
+def create_teacher():
+    try:
+        conn.execute(
+            text("INSERT INTO teacher_accounts (`first_name`, `last_name`) values (:first_name, :last_name)"),
+            request.form
+        )
+        conn.commit()
+        return render_template('teacher_create.html', error=None, success="Data inserted successfully!")
+
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('teacher_create.html', error=error, success=None)
+
+
+@app.route('/student_create', methods=['GET'])
+def create_get_request_student():
+    return render_template('student_create.html')
+
+
+@app.route('/student_create', methods=['POST'])
+def create_student():
+    try:
+        conn.execute(
+            text("INSERT INTO student_accounts (`first_name`, `last_name`) values (:first_name, :last_name)"),
+            request.form
+        )
+        conn.commit()
+        return render_template('student_create.html', error=None, success="Data inserted successfully!")
+
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('student_create.html', error=error, success=None)
+
+
+@app.route('/test_create', methods=['GET'])
+def create_get_request_test():
+    teacher_accounts = conn.execute(text("select id, concat(first_name, ' ', last_name) as name from teacher_accounts")).all()
+    return render_template('test_create.html', teacher_accounts=teacher_accounts)
+
+
+@app.route('/test_create', methods=['POST'])
+def create_test():
+    try:
+        conn.execute(
+            text("INSERT INTO quiz_questions (`teacher_id`, `question1`, `question2`, `question3`) values (:teacher_id, :question1, :question2, :question3)"),
+            request.form
+        )
+        conn.commit()
+        return render_template('test_create.html', error=None, success="Data inserted successfully!")
+
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('test_create.html', error=error, success=None)
+
+
+@app.route('/test_delete', methods=['GET'])
+def delete_get_request_test():
+    quiz_questions = conn.execute(
+        text("select id from quiz_questions")).all()
+    return render_template('test_delete.html', quiz_questions=quiz_questions)
+
+
+@app.route('/test_delete', methods=['POST'])
+def delete_test():
+    try:
+        conn.execute(
+            text("DELETE FROM quiz_questions WHERE (`id` = :id)"),
+            request.form
+        )
+        conn.commit()
+        return render_template('test_delete.html', error=None, success="Data deleted successfully!")
+
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('test_delete.html', error=error, success=None)
+
+
+@app.route('/test_edit', methods=['GET'])
+def edit_get_request_test():
+    quiz_questions = conn.execute(
+    text("select id from quiz_questions")).all()
+    return render_template('test_edit.html', quiz_questions=quiz_questions)
+
+
+@app.route('/test_edit', methods=['POST'])
+def edit_test():
+    try:
+        conn.execute(
+            text("UPDATE quiz_questions SET {} = :edit_text WHERE id = :id".format(request.form.get("questions"))),
+            request.form
+        )
+        conn.commit()
+        return render_template('test_edit.html', error=None, success="Data updated successfully!")
+
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('test_edit.html', error=error, success=None)
+
+
+@app.route('/test_take/<id>', methods=['GET'])
+def take_test_request(id = 0):
+    id = int(id)
+    student_accounts = conn.execute(
+    text("select id, concat(first_name, ' ', last_name) as name from student_accounts")).all()
+    quiz_questions = conn.execute(
+        text(f"select * from quiz_questions WHERE id = {id}")).all()
+    return render_template('test_take.html', quiz_questions=quiz_questions, student_accounts=student_accounts)
+
+@app.route('/test_take', methods=['POST'])
+def take_test():
+    try:
+        conn.execute(
+            text("INSERT INTO quiz_answers (`questions_id`, `student_id`, `answer1`, `answer2`, `answer3`) values (:questions_id, :student_id, :answer1, :answer2, :answer3)"),
+            request.form
+        )
+        conn.commit()
+        return render_template('test_take.html', error=None, success="Data inserted successfully!")
+
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('test_take.html', error=error, success=None)
 
 
 if __name__ == '__main__':
