b1d0d9b5ef90aefc1928ec6e9a89a2c47d930574
DogmaIAmGod <douglas.t.james.iii@gmail.com>
2023-05-01 12:37:49 -0400

    Re-add schema

---
+++ Schema/scammazon_products.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: scammazon
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `account_id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` longtext NOT NULL,
+  `active_warranty` enum('No','Yes') NOT NULL DEFAULT 'No',
+  `color` enum('red','orange','yellow','green','blue','purple') NOT NULL DEFAULT 'red',
+  `size` enum('small','medium','large','extra large') NOT NULL DEFAULT 'small',
+  `quantity` int NOT NULL DEFAULT '0',
+  `price` double NOT NULL DEFAULT '0',
+  PRIMARY KEY (`product_id`),
+  KEY `vendor_id_idx` (`account_id`),
+  CONSTRAINT `vendor_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1006 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1000,2,'Flabby Pants','Pants with Bell Bottoms','https://i.imgur.com/czhTk1g.jpg','No','red','small',200,205.55),(1001,4,'Blue Jeans','Jeans that are blue','https://i.imgur.com/SRoFpMp.jpeg','No','blue','small',500,206.62),(1002,4,'Blue Jeans','Jeans that are blue','https://i.imgur.com/SRoFpMp.jpeg','No','blue','medium',500,206.62),(1003,4,'Blue Jeans','Jeans that are blue','https://i.imgur.com/SRoFpMp.jpeg','No','blue','large',500,206.62),(1004,4,'Blue Jeans','Jeans that are blue','https://i.imgur.com/SRoFpMp.jpeg','No','blue','extra large',500,306.72);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-01 12:37:19
